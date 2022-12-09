ff8cee48fae0c7828c0f2b0be557102594135e50
rnfltpgus <jpp901220@gmail.com>
2022-12-09 17:33:58 +0900

    Updates

---
+++ component---src-pages-404-js-b1cdd137fc32fa5e087c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkJungLog=self.webpackChunkJungLog||[]).push([[883],{45:function(e,t,n){n.r(t);var u=n(7294),l=n(2758),a=n(3432);t.default=function(){return u.createElement(l.Z,null,u.createElement(a.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b1cdd137fc32fa5e087c.js.map
