539521f8935fd06c78568c55556ee3ed5f71faa7
jungseahyun <sh.jung@towncar.co.kr>
2023-02-06 10:18:17 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c4677967dc5aeeb139ff.js"
+    "url": "webpack-runtime-51e291759c658b0dd903.js"
   },
   {
     "url": "styles.c75940ba2e9d08c2bb15.css"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2ac8ad4141f6f32e2a040dec402ccc26"
+    "revision": "baa5c8ee6e383b136c9db8b66fa94a2f"
   },
   {
     "url": "static/webfonts/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
